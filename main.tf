e5732e881bc31264bf306433d1f44beb633e9d90
Sridhar <sridhar7443@gmail.com>
2023-03-31 14:04:22 +0530

    new file

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-data "azurerm_resources" "example8143" {
+data "azurerm_resources" "Sridhartcs12345" {
   resource_group_name = "example-resources8143"
 }
 
-# Get Resources with specific Tags
-data "azurerm_resources" "example8143" {
-  resource_group_name = "example-resources8143"
-
-  required_tags = {
-    environment = "production"
-    role        = "webserver"
-  }
-}
