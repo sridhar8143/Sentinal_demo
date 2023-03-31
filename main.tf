54125ad39670493d1aec9f91049009ef4012fd3b
Sridhar <sridhar7443@gmail.com>
2023-03-31 14:42:34 +0530

    new file

--- main.tf
+++ main.tf
@@ -17,7 +17,44 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "kdjnalskdlaskmclmksmc"
+
+resource "azurerm_resource_group" "Sridhar_march1" {
+  name     = "example-resources"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "Sridhar_tcs12345"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "new_policy_check123456" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "policy_check_test" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  threat_detection_policy {
+    state = "Disabled"
+  }
+
+  tags = {
+    foo = "bar"
+  }
 }
