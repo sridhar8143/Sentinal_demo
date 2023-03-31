93467a2991cbdfab26086d73850e329ec39ff679
Sridhar <sridhar7443@gmail.com>
2023-03-31 14:16:21 +0530

    new file

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
-
 provider "azurerm" {
-  features {}
+  features {
+    
+  }
+  
 }
 
-provider "azurerm" {
-  features {}
-  
-  subscription_id="f13ee35e-698e-4e96-9c57-6ff72c0da322"
-  tenant_id="f6981b0a-3915-4628-be7e-368196415f8f"
-  client_id="94360f58-5bed-4f81-b199-bb60611fc76a"
-  client_secret="PMe8Q~PiTSMIl1AMnlWQahTwiKysBvCjsFxtuaU7"
-
+resource "azurerm_resource_group" "example" {
+  name     = "Sridhar_tcs8143"
+  location = "West Europe"
 }
